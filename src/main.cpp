12ad0a48f6d0b7927209e5365c0f9e7da86f4cc0
Tehtehteh <tehfvpr@gmail.com>
2019-04-25 23:13:51 +0300

    Added glew stuff to CMakeLists.

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,23 @@
+#include "glew.h"
 #include <GLFW/glfw3.h>
-#include <GL/glew.h>
-#include <stdio.h>
+#include <cstdio>
 #include <cstdlib>
 #include <iostream>
 #include <app/Application.h>
 #include "utils/utils.h"
 
-
 int main() {
     const int width = 640;
     const int height = 480;
     std::string name = "My test application";
-    Application app(height, width, name.c_str());
+    printf("Starting glew Init\n");
     GLenum err = glewInit();
     if (GLEW_OK != err) {
         fprintf(stderr, "Error initializing glew. Msg: %d", err);
         return 1;
     }
     fprintf(stdout, "Status: Using GLEW %s\n", glewGetString(GLEW_VERSION));
+    Application app(height, width, name.c_str());
     app.loop();
     Application::cleanup();
     return 0;
